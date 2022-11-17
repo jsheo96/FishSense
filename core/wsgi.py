d4eda48f6a85932f09e3d1310719c1ee22452496
Jiseong Heo <gjwltjd96@gmail.com>
2022-11-17 19:22:57 +0900

    wsgi edidt for deploying to apache2

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,7 +3,10 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-import os
+import os,sys
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
